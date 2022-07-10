a88c04325e4b5694bef16dce8f39d744bfe34f8e
raulco1 <racorrea@ucsd.edu>
2022-07-10 13:44:43 -0700

    Website counter created

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,31 @@
 // attempt at a quine
 document.getElementById("quine").innerHTML = document.documentElement.innerHTML.replace(/\&/g, "&amp;").replace(/\</g, "&lt;").replace(/\>/g, "&gt;");
-// Make it so it resize text to fit within webpage 
+// Make it so it resize text to fit within webpage
+
+
+// //Website Counter
+// var counterContainer = document.querySelector(".website-counter");
+// var visitCount = localstorage.getitem("page_view");
+//
+// visitCount = 1;
+// localstorage.setItem("page_view",1)
+//
+// visitCount = Number(visitCount) + 1;
+// localstorage.setItem("page_view", visitCount);
+//
+// counterContainer.innerHTML = visitCount;
+
+
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
